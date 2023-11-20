30b1c8060c3e37c4acb2bfed8063aa581749363f
Juan Guillermo Buitrago Calle <64021772+Juanbc2@users.noreply.github.com>
2023-11-20 00:37:29 -0500

    localhost again

--- service/url.ts
+++ service/url.ts
@@ -1,5 +1,5 @@
 //DEV
-//export const api_url = "http://localhost:3000";
+export const api_url = "http://localhost:3000";
 
 //PROD
-export const api_url = "https://gestion-de-inventarios-orpin.vercel.app";
+//export const api_url = "https://gestion-de-inventarios-orpin.vercel.app";
