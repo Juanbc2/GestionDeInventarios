c9d7de290fed05611e5a6f80c973a9b79377b830
Juan Guillermo Buitrago Calle <64021772+Juanbc2@users.noreply.github.com>
2023-11-23 12:20:19 -0500

    button type error

--- components/ui/DialogButton.tsx
+++ components/ui/DialogButton.tsx
@@ -4,29 +4,25 @@ import { useState } from "react";
 interface DialogButtonProps {
   text: string;
   onClick: () => void;
-  type?: "button" | "submit" | undefined;
 }
 
-const DialogButton = ({ text, onClick, type }: DialogButtonProps) => {
+const DialogButton = ({ text, onClick }: DialogButtonProps) => {
   const [isCreateButtonDisabled, setIsCreateButtonDisabled] = useState(false);
 
   return (
     <button
-    className="m-5 py-2 px-5 rounded-lg text-slate-50 bg-[#10aeb9] hover:bg-[#05965a] font-bold text-lg"
-    style={{ fontFamily: 'Copperplate Gothic, serif', fontWeight: 'bold' }}
-    onClick={() => {
-      setIsCreateButtonDisabled(true);
-      onClick();
-    }}
-  >
-    {isCreateButtonDisabled ? (
-      <CircularProgress size={15} color="inherit" />
-    ) : null}{" "}
-    {text}
-  </button>
-  
-
-
+      className="m-5 py-2 px-5 rounded-lg text-slate-50 bg-[#10aeb9] hover:bg-[#05965a] font-bold text-lg"
+      style={{ fontFamily: "Copperplate Gothic, serif", fontWeight: "bold" }}
+      onClick={() => {
+        setIsCreateButtonDisabled(true);
+        onClick();
+      }}
+    >
+      {isCreateButtonDisabled ? (
+        <CircularProgress size={15} color="inherit" />
+      ) : null}{" "}
+      {text}
+    </button>
   );
 };
 
